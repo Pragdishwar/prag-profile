ee67349333946c848110722dfce95c4334ddbe4c
Pragdishwar <pragdishwar@gmail.com>
2026-09-19 18:49:17 +0530

    fix: removed scroll delay in lenis by using lerp

--- src/components/ui/SmoothScroll.tsx
+++ src/components/ui/SmoothScroll.tsx
@@ -1,4 +1,4 @@
-﻿'use client';
+'use client';
 
 import { useEffect } from 'react';
 import Lenis from '@studio-freight/lenis';
@@ -6,12 +6,11 @@ import Lenis from '@studio-freight/lenis';
 export default function SmoothScroll({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.2,
-      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+      lerp: 0.08,
       orientation: 'vertical',
       gestureOrientation: 'vertical',
       smoothWheel: true,
-      wheelMultiplier: 1,
+      wheelMultiplier: 1.2,
       touchMultiplier: 2,
     });
 
